ea9413af7483dddfb24de6b1204e319eb0e594be
xuzeyang <xuzeyang@bytedance.com>
2021-07-09 18:19:07 +0800

    update

--- re.ts
+++ re.ts
@@ -1,5 +1,3 @@
-import assert from "assert";
-
 interface RegularExpression {};
 
 class Re {
@@ -10,18 +8,112 @@ class Re {
   get exp() {
     return this.ex
   }
+  get nfa() {
+    return exp2nfa(this.ex)
+  }
   iterate() {
     this.ex = new IterationExpression(this.ex)
     return this
   }
-  union(anotherEx: RegularExpression) {
-    this.ex = new UnionExpression(this.ex, anotherEx)
+  union(anotherEx: Re) {
+    this.ex = new UnionExpression(this.ex, anotherEx.exp)
     return this
   }
-  concat(anotherEx: RegularExpression) {
-    this.ex = new ConcatenationExpression(this.ex, anotherEx)
+  concat(anotherEx: Re) {
+    this.ex = new ConcatenationExpression(this.ex, anotherEx.exp)
     return this
   }
+}
+
+export function exp2nfa (exp: RegularExpression): NFA {
+  const end = {
+    exp,
+    transformTable: null
+  }
+  if (exp instanceof SymbolExpression) {
+    const start = {
+      exp,
+      transformTable: new Map().set(exp, end)
+    }
+    return {
+      start,
+      end
+    }
+  }
+  if (exp instanceof UnionExpression) {
+    const nfaA = exp2nfa(exp.exA)
+    const nfaB = exp2nfa(exp.exB)
+    const map = new Map()
+
+    nfaA.end.transformTable = new Map().set(new EmptyExpression(), end)
+    nfaB.end.transformTable = new Map().set(new EmptyExpression(), end)
+
+    map.set(new EmptyExpression(), nfaA.start)
+    map.set(new EmptyExpression(), nfaB.start)
+
+    const start = {
+      exp,
+      transformTable: map
+    }
+
+    return {
+      start,
+      end
+    }
+  }
+  if (exp instanceof ConcatenationExpression) {
+    const nfaA = exp2nfa(exp.exA)
+    const nfaB = exp2nfa(exp.exB)
+    const map = new Map()
+
+    nfaA.end.transformTable = new Map().set(new EmptyExpression(), nfaB.end)
+    nfaB.end.transformTable = new Map().set(new EmptyExpression(), end)
+
+    map.set(new EmptyExpression(), nfaA.start)
+
+    const start = {
+      exp,
+      transformTable: map
+    }
+
+    return {
+      start,
+      end
+    }
+  }
+  if (exp instanceof IterationExpression) {
+    const nfa = exp2nfa(exp.ex)
+    const startMap = new Map()
+    const start = {
+      exp,
+      transformTable: startMap
+    }
+
+    nfa.end.transformTable = new Map()
+      .set(new EmptyExpression(), start)
+      .set(new EmptyExpression(), end)
+
+    startMap.set(new EmptyExpression(), nfa.start)
+    startMap.set(new EmptyExpression(), end)
+
+    return {
+      start,
+      end
+    }
+  }
+  throw Error('invalid regular expression')
+}
+
+type TransformTable = Map<EmptyExpression | SymbolExpression, NFAState> | null;
+
+interface NFAState {
+  exp: RegularExpression;
+  transformTable: TransformTable;
+}
+
+interface NFA {
+  start: NFAState;
+  end: NFAState
 }
 
 class UnionExpression implements RegularExpression {
@@ -58,75 +150,10 @@ class IterationExpression implements RegularExpression {
   }
 };
 
-interface AcceptingState {};
-interface ErrorState {};
-
-interface NFAState {
-  exp: RegularExpression;
-  transformTable: Map<EmptyExpression | SymbolExpression, NFAState> | null;
-}
-
-interface NFA {
-  start: NFAState;
-  end: NFAState
-}
-
-function exp2NFA (exp: RegularExpression): NFA {
-  if (exp instanceof SymbolExpression) {
-    const end = {
-      exp,
-      transformTable: null
-    }
-    const start = {
-      exp,
-      transformTable: new Map().set(exp, end)
-    }
-    return {
-      start,
-      end
-    }
-  }
-  if (exp instanceof UnionExpression) {
-    const nfaA = exp2NFA(exp.exA)
-    const nfaB = exp2NFA(exp.exB)
-    const map = new Map()
-
-    const end = {
-      exp,
-      transformTable: null
-    }
-
-    nfaA.end.transformTable = new Map().set(new EmptyExpression(), end)
-    nfaB.end.transformTable = new Map().set(new EmptyExpression(), end)
-
-    map.set(new EmptyExpression(), nfaA.start)
-    map.set(new EmptyExpression(), nfaB.start)
-
-    const start = {
-      exp,
-      transformTable: map
-    }
-
-    return {
-      start,
-      end
-    }
-  }
-  throw Error('invalid regular expression')
-}
-
-const re = {
+export const re = {
   Symbol: (a: string) => new Re(new SymbolExpression(a)),
 }
 
-const res = re.Symbol('a').concat(re.Symbol('b').iterate()).exp
+const res = re.Symbol('a').concat(re.Symbol('b').iterate())
 
-if(res instanceof ConcatenationExpression) {
-  console.log('concat!')
-} else if (res instanceof SymbolExpression) {
-  console.log('ooo')
-} else {
-  console.log('wat')
-}
-
-console.log('compiler')
+console.log(res.nfa)
